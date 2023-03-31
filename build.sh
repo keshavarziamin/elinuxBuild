8f7f92f0e29691db5728993059b979eebe53e73b
Amin <keshavarzi.amin>
2023-03-31 15:12:46 +0330

    fix bug of buildroot list

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+
 source error_handler/errhdl.sh
-source buildtools/listboars.sh
-source buildtools/buildroot.sh
+source buildtools/buildroot/buildroot.sh
 source buildtools/install_essential.sh
 
 YP="yocto"
@@ -18,14 +18,17 @@ BOARD=""
 
 function usage() {
     echo "USAGE: arguments in not valid "
-
 }
 
 function build_run() {
+
+    # exit error if arguments are not valid
     if [ $# -ne 2 ]; then
         usage 1>&3
         echo_return $ERROR "The arguments are wrongs"
     fi
+
+    #select build system
     case $1 in
     $BR)
         echo "buildroot ${2}"
@@ -33,35 +36,43 @@ function build_run() {
         ;;
     *)
         echo_return $ERROR "please choose a correct build system"
+        ;;
     esac
 }
 
-while getopts ":b:d:l" option; do
-    case $option in
-    b)
-        if [ $OPTARG != $YP ] && [ $OPTARG != $BR ] && [ $OPTARG != $MB ]; then
+function get_options() {
+
+    while getopts "b:d:l" option; do
+
+        case $option in
+        b)
+            if [ $OPTARG != $YP ] && [ $OPTARG != $BR ] && [ $OPTARG != $MB ]; then
+                usage
+                exit 1
+            fi
+            BUILD_SYSTEM=$OPTARG
+            ;;
+        d)
+            debug checkBoard $OPTARG
+            BOARD=$OPTARG
+            ;;
+        l)
+            cat $LIST_TEXT_DIR 1>&3
+            exit 0
+            ;;
+        *)
+            echo_err "invalid arguments"
             usage
             exit 1
-        fi
-        BUILD_SYSTEM=$OPTARG
-        ;;
-    d)
-        debug checkBoard $OPTARG
-        BOARD=$OPTARG
-        ;;
-    l)
-        cat $LIST_TEXT_DIR 1>&3
-        echo
-        exit 0
-        ;;
-    \?)
-        echo "invalid arguments"
-        exit 1
-        ;;
-    esac
-done
+            ;;
+        esac
+    done
 
-debug install_essential
+}
+
+get_options $@
+
+# debug install_essential
 debug build_run $BUILD_SYSTEM $BOARD
 
 exit 0
