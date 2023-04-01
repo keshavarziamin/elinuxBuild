1afec9950372dbad65d6946137bd01b40b159564
Amin <keshavarzi.amin>
2023-04-01 19:02:06 +0330

    fix bug of build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@ BUILDROOT_DIR=buildtools/buildroot
 source ${BUILDROOT_DIR}/buildroot.sh
 source buildtools/install_essential.sh
 
-
 function usage() {
     echo "USAGE: arguments in not valid "
 }
 
-
-VALID_ARGS=$(getopt -o B: --long buildroot: -- "$@")
+VALID_ARGS=$(getopt -o :B: --long buildroot: -- "$@")
 
 if [ $? -ne 0 ]; then
     echo_err "the arguments are not valid"
@@ -21,7 +19,13 @@ eval set -- "$VALID_ARGS"
 
 case "$1" in
 -B | --buildroot)
+    shift
     buildroot_run $@
+    ;;
+: | * | \?)
+    echo_err "The arguments are not valid"
+    usage
+    exit ${ERROR}
     ;;
 esac
 exit 0
